e3eca928519fefeb7f3a56ba4a3ebf397e3a2fb9
Matt Coppola <matt.coppola@gmail.com>
2019-01-16 17:50:42 -0600

    action tests completed

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -1,26 +1,32 @@
-// import {ADD_LIST, addList, ADD_CARD, addCard} from './index';
-//
-// describe('addList', () => {
-//     it('Should return the action', () => {
-//         const title = 'List title';
-//         const action = addList(title);
-//         expect(action.type).toEqual(ADD_LIST);
-//         expect(action.title).toEqual(title);
-//     });
-// });
-//
-// describe('addCard', () => {
-//     it('Should return the action', () => {
-//         const text = 'Card text';
-//         const listIndex = 10;
-//         const action = addCard(text, listIndex);
-//         expect(action.type).toEqual(ADD_CARD);
-//         expect(action.text).toEqual(text);
-//         expect(action.listIndex).toEqual(listIndex);
-//     });
-// });
+import {
+  REQUEST,
+  request,
+  SIGNUP_USER,
+  loggedIn,
+  LOGIN_USER,
+  loginUser,
+  LOGOUT_USER,
+  logOutUser,
+  RETRIEVE_WORKORDERS,
+  displayWorkOrders,
+  RETRIEVE_INVENTORY,
+  displayInventory,
+  AUTH_REQUEST,
+  authRequest,
+  SET_AUTH_TOKEN,
+  setAuthToken,
+  AUTH_SUCCESS,
+  authSuccess,
+  ERROR,
+  fetchErr
+} from '../actions/index';
 
-import { SIGNUP_USER, loggedIn, LOGIN_USER, loginUser } from '../actions/index';
+describe ('request', () => {
+  it('Should return the action', () => {
+    const action = request();
+    expect(action.type).toEqual(REQUEST);
+  });
+});
 
 describe ('signupUser', () => {
   it('Should return the action', () => {
@@ -35,5 +41,64 @@ describe ('loginUser', () => {
     const action = loginUser(user);
     expect(action.type).toEqual(LOGIN_USER);
     expect(action.user).toEqual(user);
+  });
+});
+
+describe ('logOutUser', () => {
+  it('Should return the action', () => {
+    const action = logOutUser();
+    expect(action.type).toEqual(LOGOUT_USER);
+  });
+});
+
+describe ('displayWorkOrders', () => {
+  it('Should return the action', () => {
+    const results = {name: 'name', caseNumber: '12345'};
+    const action = displayWorkOrders(results);
+    expect(action.type).toEqual(RETRIEVE_WORKORDERS);
+    expect(action.results).toEqual(results);
+  });
+});
+
+describe ('displayInventory', () => {
+  it('Should return the action', () => {
+    const results = 'Inventory Item';
+    const action = displayInventory(results);
+    expect(action.type).toEqual(RETRIEVE_INVENTORY);
+    expect(action.results).toEqual(results);
+  });
+});
+
+describe ('authRequest', () => {
+  it('Should return the action', () => {
+    const action = authRequest();
+    expect(action.type).toEqual(AUTH_REQUEST);
+  });
+});
+
+describe ('setAuthToken', () => {
+  it('Should return the action', () => {
+    const authToken = 'token';
+    const action = setAuthToken(authToken);
+    expect(action.type).toEqual(SET_AUTH_TOKEN);
+    expect(action.authToken).toEqual(authToken);
+  });
+});
+
+describe ('authSuccess', () => {
+  it('Should return the action', () => {
+    const currentUser = 'user';
+    const action = authSuccess(currentUser);
+    expect(action.type).toEqual(AUTH_SUCCESS);
+    expect(action.currentUser).toEqual(currentUser);
+  });
+});
+
+describe ('fetchErr', () => {
+  it('Should return the action', () => {
+    const err = 'err';
+    const action = fetchErr(err);
+    expect(action.type).toEqual(ERROR);
+    expect(action.err).toEqual(err);
   });
 });
