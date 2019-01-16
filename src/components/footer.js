81de32830202e66e5985fb201ed631177f11b558
Matt Coppola <matt.coppola@gmail.com>
2019-01-15 20:58:17 -0600

    added border to workorder and inventory lists, commented out footer links that go nowhere (about me, twitter, facebook, and instagram)

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,13 +9,13 @@ export class Footer extends React.Component {
             <footer role="contentinfo">
                 <a onClick={() => this.props.backToTop} className="btn btn-full scroll" href="#home">Back to top</a>
                 <div className="row">
-                    <a className="aboutUs" href="/">About Us</a>
+                    {/*<a className="aboutUs" href="/">About Us</a>*/}
                     <ul className="social-links">
                         <li><a href="https://github.com/mattCoppola" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" /></a></li>
-                        <li><i className="fab fa-facebook" /></li>
-                        <li><a href="/"><i className="fab fa-linkedin-in" /></a></li>
-                        <li><a href="/"><i className="fab fa-twitter" /></a></li>
-                        <li><a href="/"><i className="fab fa-instagram" /></a></li>
+                        {/*<li><i className="fab fa-facebook" /></li>*/}
+                        <li><a href="https://www.linkedin.com/in/matt-coppola-81b012177/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in" /></a></li>
+                        {/*<li><a href="/"><i className="fab fa-twitter" /></a></li>*/}
+                        {/*<li><a href="/"><i className="fab fa-instagram" /></a></li>*/}
                     </ul>
                 </div>
             </footer>
--- src/components/workorderform.js
+++ src/components/workorderform.js
@@ -61,7 +61,7 @@ export class WorkorderForm extends React.Component {
                         <li className="workorder-li">Click the button to open up a new workorder form.</li>
                         <li className="workorder-li">Enter all of your information and click save</li>
                         <li className="workorder-li">Once saved you can see your work order listed in the Completed Work Orders table below.</li>
-                        <li className="workorder-li">The quantity of the innventory part you selected for repair is deducted from the Inventory table.</li>
+                        <li className="workorder-li">The quantity of the inventory part you selected for repair is deducted from the Inventory table.</li>
                     </ul>
                     <input onClick={this.toggleHidden} className="new-work-order" type="submit" value="Create Work Order" />
                 </div>
